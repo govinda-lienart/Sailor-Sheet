0df33f7541c812f117b85e24fd86331dc5ef8722
Govinda Lienart <69435075+govinda-lienart@users.noreply.github.com>
2025-09-10 17:31:47 +0200

    Working app v21 [chat]
    restructure

--- Context/exercise1.py
+++
@@ -1,8 +0,0 @@
-def get_file_name (name):
-    return f'comfirming that your file name is {name}'
-
-def file_name_extension (name, extension):
-    return f'comfirming that your file name is {name} with the following extension {extension}'
-
-print (get_file_name ("guidelines"))
-print (file_name_extension ("guidelines", "docx"))
